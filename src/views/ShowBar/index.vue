e4616053b1ae235e2b3b581ded70acd3fab6955e
hyqya <15538218453@163.com>
2022-03-22 08:53:42 +0800

    .

---
+++ src/views/ShowBar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'show-bar'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/SportBar/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'sport-bar'
+}
+</script>
+
+<style>
+
+</style>
